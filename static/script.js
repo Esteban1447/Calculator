7b130e77461cb6170e7815d6a6c09a1c855b989d
Esteban1447 <esteban.robledo.rc@gmail.com>
2026-07-10 20:27:50 -0500

    Se creo suma de 2 numeros, se comento y se empezo a hacer la de todos

--- static/script.js
+++ static/script.js
@@ -1,52 +1,106 @@
 const btns = document.querySelectorAll(".btn");
 const display = document.getElementById("display");
-const token = []
+const token = [];
 
 let expresion = "";
-let number = ""
+let number = "";
 
 function calculator() {
-    btns.forEach((element) => {
-        element.addEventListener("click", () => {
-            const value = element.textContent;
-            
+  btns.forEach((element) => {
+    element.addEventListener("click", () => {
+      const value = element.textContent;
 
-            if(value === "C"){
-                expresion = ""
-                display.value = expresion
-            }else if(value === "="){
-                console.log(expresion)
+      if (value === "C") {
+        expresion = "";
+        display.value = expresion;
+      } else if (value === "=") {
+        console.log(expresion);
+        token.length = 0;
+        number = "";
 
-
-                token.length = 0;
-                number = "";
-                
-                for (const character of expresion){
-                if (!isNaN(character)){
-                number += character
-                }else{
-                    token.push(number);
-                    token.push(character);
-                    number = ""
-                }
-                console.log(token)
-
+        for (const character of expresion) {
+          if (!isNaN(character)) {
+            number += character;
+          } else {
+            token.push(number);
+            token.push(character);
+            number = "";
+          }
         }
-            if(number != ""){
-                token.push(number)
+
+        if (number != "") {
+          token.push(number);
+        }
+
+        console.log(token);
+
+        let answerd = Number(token[0]);
+
+        for (let i = 1; i < token.length; i += 2) {
+          let operator = token[i];
+          let number = Number(token[i + 1]);
+
+          console.log(operator);
+          console.log(number);
+
+          if ((operator = !"")) {
+            switch (operator) {
+              case "+":
+                answerd = number + Next;
+                break;
+              case "-":
+                answerd = number - answerd;
+                break;
+              case "/":
+                answerd = number / answerd;
+                break;
+              case "*":
+                answerd = number * answerd;
+                break;
+              default:
+                answerd = "No se pudo resolver";
+                break;
             }
-        
-            }else{
-                expresion += value;
-                display.value = expresion;
-            }
+          }
+        }
 
-        });
+        // let number1 = Number(token[0]);
+        // let number2 = Number(token[2]);
+        // let operator = String(token[1])
+        // let answerd = 0;
 
-        
+        // console.log(token[1])
+        // console.log(token[2]);
+        // console.log(operator);
+        // console.log(number2 + number1);
+        // console.log(number1);
 
+        // switch (operator) {
+        //   case "+":
+        //     answerd = number1 + number2;
+        //     break
+        //   case "-":
+        //     answerd = number1 - number2;
+        //     break
+        //   case "/":
+        //     answerd = number1 / number2;
+        //     break
+        //   case "*":
+        //     answerd = number1 * number2;
+        //     break
+        //   default:
+        //     answerd = "No se pudo resolver";
+        //     break
+        // }
 
+        console.log(answerd);
+        display.value = answerd;
+      } else {
+        expresion += value;
+        display.value = expresion;
+      }
     });
+  });
 }
 
 calculator();
