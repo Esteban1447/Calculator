0e65a20c78304abcb91d1425280735540f5d41cd
Esteban1447 <esteban.robledo.rc@gmail.com>
2026-07-15 19:16:36 -0500

    Añadir la verificacion de puntos

--- static/script.js
+++ static/script.js
@@ -23,8 +23,9 @@ function calculator() {
         if (operadores.includes(last)) {
           return;
         }
+
         for (const character of expresion) {
-          if (!isNaN(character)) {
+          if (!isNaN(character) || character == ".") {
             number += character;
           } else {
             token.push(number);
@@ -53,68 +54,38 @@ function calculator() {
                 token.splice(i - 1, 3, answerd);
                 i -= 2;
                 break;
+
               case "/":
                 console.log("Entre a la division");
                 answerd = number1 / number2;
                 token.splice(i - 1, 3, answerd);
                 i -= 2;
                 break;
-              default:
-                break;
             }
             console.log(token);
           }
         }
+
         answerd = Number(token[0]);
 
         for (let i = 1; i < token.length; i += 2) {
           let operator = token[i];
           let number = Number(token[i + 1]);
 
-          console.log(operator);
-          console.log(number);
-
           switch (operator) {
             case "+":
               answerd = number + answerd;
               break;
+
             case "-":
               answerd = answerd - number;
               break;
+
             default:
               answerd = "No se pudo resolver";
               break;
           }
         }
-
-        // let number1 = Number(token[0]);
-        // let number2 = Number(token[2]);
-        // let operator = String(token[1])
-        // let answerd = 0;
-
-        // console.log(token[1])
-        // console.log(token[2]);
-        // console.log(operator);
-        // console.log(number2 + number1);
-        // console.log(number1);
-
-        // switch (operator) {
-        //   case "+":
-        //     answerd = number1 + number2;
-        //     break
-        //   case "-":
-        //     answerd = number1 - number2;
-        //     break
-        //   case "/":
-        //     answerd = number1 / number2;
-        //     break
-        //   case "*":
-        //     answerd = number1 * number2;
-        //     break
-        //   default:
-        //     answerd = "No se pudo resolver";
-        //     break
-        // }
 
         console.log(answerd);
         display.value = answerd;
@@ -132,10 +103,34 @@ function calculator() {
 
         if (operadores.includes(value) && operadores.includes(last)) {
           return;
-        } else {
-          expresion += value;
-          display.value = expresion;
         }
+
+        // ------------------------------
+        // VALIDAR PUNTOS DECIMALES
+        // ------------------------------
+        if (value === ".") {
+          let numeroActual = "";
+
+          // Vamos hacia atrás buscando el número actual
+          for (let i = expresion.length - 1; i >= 0; i--) {
+            let caracter = expresion[i];
+
+            // Si encontramos un operador, paramos
+            if (operadores.includes(caracter)) {
+              break;
+            }
+
+            // Construimos el número actual
+            numeroActual = caracter + numeroActual;
+          }
+
+          if (numeroActual.includes(".")) {
+            return;
+          }
+        }
+
+        expresion += value;
+        display.value = expresion;
       }
     });
   });
